5f98f6aa891a60e631d22bb5272d1ab698ca5246
Bogdan <bogdan7morozyuk@gmail.com>
2020-05-25 13:21:44 +0300

    refactor: add card component

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Card, CardContent, Typography, Grid } from "@material-ui/core";
-import CountUp from "react-countup";
-import isEmpty from "lodash/fp/isEmpty";
-import cx from "classnames";
+import Card from "src/components/cards/Card";
+import { Grid, Typography } from "@material-ui/core";
 import styles from "src/components/cards/Cards.module.css";
+import isEmpty from "lodash/fp/isEmpty";
 
 const Cards = ({ data }) => {
   const { confirmed, recovered, deaths, lastUpdate } = data;
 
   const lastUpdatedAt = new Date(lastUpdate).toDateString();
+
+  const datasets = [
+    {
+      title: "Infected",
+      value: confirmed?.value,
+      variant: "infected",
+      description: "Number of active cases of Covid-19",
+      lastUpdatedAt
+    },
+    {
+      title: "Recovered",
+      value: recovered?.value,
+      variant: "recovered",
+      description: "Number of recoveries from Covid-19",
+      lastUpdatedAt
+    },
+    {
+      title: "Deaths",
+      value: deaths?.value,
+      variant: "deaths",
+      description: "Number of deaths by Covid-19",
+      lastUpdatedAt
+    },
+  ];
 
   if (isEmpty(data)) {
     return (
@@ -19,57 +42,9 @@ const Cards = ({ data }) => {
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.infected])}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Infected
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={confirmed.value}
-                duration={3}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">{lastUpdatedAt}</Typography>
-            <Typography variant="body2">Number of active cases of Covid-19</Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.recovered])}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Recovered
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={recovered.value}
-                duration={3}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">{lastUpdatedAt}</Typography>
-            <Typography variant="body2">Number of recoveries from Covid-19</Typography>
-          </CardContent>
-        </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx([styles.card, styles.deaths])}>
-          <CardContent>
-            <Typography color="textSecondary" gutterBottom>
-              Deaths
-            </Typography>
-            <Typography variant="h5">
-              <CountUp
-                start={0}
-                end={deaths.value}
-                duration={3}
-                separator=","
-              />
-            </Typography>
-            <Typography color="textSecondary">{lastUpdatedAt}</Typography>
-            <Typography variant="body2">Number of deaths by Covid-19</Typography>
-          </CardContent>
-        </Grid>
+        {datasets.map(dataset => (
+          <Card {...{ key: dataset.title, ...dataset }} />
+        ))}
       </Grid>
     </div>
   );
